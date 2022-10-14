aea72f83b9e2027a85fcc037afc05f6e8e748e35
luisgobo <lagbtech@hotmail.com>
2022-10-14 13:13:28 -0600

    Redirect error not found added

--- src/components/general/ErrorPage.tsx
+++ src/components/general/ErrorPage.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { IErrorInfo } from '../../models/IErrorInfo';
 import '../../styles/errorInfo.css'
 
-
 export interface ItemErrorProps {
     errorInfo: (IErrorInfo);
 }
@@ -14,14 +13,12 @@ export const ErrorPage: React.FC<ItemErrorProps> = ({
     return (
         <>
             <div className='special-bg-color'>
-
                 <div >
                     <div className="starsec"></div>
                     <div className="starthird"></div>
                     <div className="starfourth"></div>
                     <div className="starfifth"></div>
                 </div>
-
                 <div className="lamp__wrap">
                     <div className="lamp">
                         <div className="cable"></div>
@@ -32,9 +29,7 @@ export const ErrorPage: React.FC<ItemErrorProps> = ({
                         <div className="light"></div>
                     </div>
                 </div>
-                {/* <!-- END Lamp --> */}
                 <section className="error">
-                    {/* <!-- Content --> */}
                     <div className="error__content">
                         <div className="error__message message">                            
                             <h1 className="message__title">{errorInfo.errorCode}</h1>
@@ -45,13 +40,8 @@ export const ErrorPage: React.FC<ItemErrorProps> = ({
                             <a href="/" target="_blanck" className="e-nav__link"></a>
                         </div>
                     </div>
-                    {/* <!-- END Content --> */}
-
                 </section>
-
             </div>
-
         </>
-
     )
 };
--- src/pages/RouterLinks.tsx
+++ src/pages/RouterLinks.tsx
@@ -3,22 +3,29 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Home";
 import Settings from "./Settings";
 import { Tarea01Funciones } from "./Tareas/Tarea01/Tarea01Funciones";
-import Tarea01Clases        from "./Tareas/Tarea01/Tarea01Clases";
+import Tarea01Clases from "./Tareas/Tarea01/Tarea01Clases";
 import { Tarea02 } from "./Tareas/Tarea02/Tarea02";
+import { ErrorPage } from "../components/general/ErrorPage";
+import { IErrorInfo } from "../models/IErrorInfo";
 
-const RouterLinks = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/HomePage" element={<HomePage />} />                
-        <Route path="/Settings" element={<Settings />} />
-        <Route path="/Tarea01/funciones" element={<Tarea01Funciones />} />
-        <Route path="/Tarea01/clases" element={<Tarea01Clases />} />
-        <Route path="/Tarea02" element={<Tarea02/>} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+const errorInfo: IErrorInfo ={
+  errorCode: 404,
+  errorDescription: "Page Not found"
+}
+
+const RouterLinks = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/HomePage" element={<HomePage />} />
+      <Route path="/Settings" element={<Settings />} />
+      <Route path="/Tarea01/funciones" element={<Tarea01Funciones />} />
+      <Route path="/Tarea01/clases" element={<Tarea01Clases />} />
+      <Route path="/Tarea02" element={<Tarea02 />} />
+      <Route path="*" element={<ErrorPage errorInfo={errorInfo} />} />
+
+    </Routes>
+  </BrowserRouter>
+);
 
 export default RouterLinks;
